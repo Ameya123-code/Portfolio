c71f55871f883139527ff1e654aff7a217e83007
Ameya123-code <79597757+Ameya123-code@users.noreply.github.com>
2025-11-24 07:41:45 +0530

    Update App.jsx To show Experiences

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
       <Hero />
       <About />
     {/*  <Projects />    */}
-    {/*  <Experiences /> */}
+      <Experiences /> 
     {/*  <Testimonial /> */}
       <Contact />
       <Footer/>
